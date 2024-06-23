c17397c1ba2470c925ff40dbd96fc5814350763a
onurcaner <109483673+onurcaner@users.noreply.github.com>
2024-06-24 01:35:22 +0300

    -

---
+++ The Wild Oasis Website/.prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+};
